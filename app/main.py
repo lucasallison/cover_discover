48502ebf33a991b0b71e5b60ecbb33338c118434
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2023-11-30 14:09:47 +0100

    app: fix after 8a5f69c21d32cba1dd2cf3ea3cb3df3146ec0850 and use project folders

--- app/main.py
+++ app/main.py
@@ -5,16 +5,38 @@ import sys
 import time
 from sys import platform
 
+def help():
+    print(f'Usage: python {sys.argv[0]} <project name> [midi file]', file=sys.stderr)
+    print('If no MIDI file is given, the program starts with an empty score.', file=sys.stderr)
+    sys.exit(1)
+
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 
 mscore = 'mscore'
 if platform == 'darwin':
     mscore = '/Applications/MuseScore 3.app/Contents/MacOS/mscore'
 
+if len(sys.argv) <= 1:
+    print('Not enough arguments given.\n', file=sys.stderr)
+    help()
+
+project_name = sys.argv[1]
+project_dir = f'{curr_dir}/{project_name}'
+os.system(f'mkdir -p "{project_dir}"')
 
 fiter = 0
 def get_score_file():
-    return f'{curr_dir}/score_{fiter}.mscz'
+    return f'{project_dir}/score_{fiter}.mscz'
+
+if len(sys.argv) > 2:
+    # Initial conversion of the input midi file to the first score file
+    midi_file = sys.argv[2]
+    convert(midi_file, get_score_file())
+else:
+    # If no MIDI file is given, start with empty project.
+    os.system(f"cp {curr_dir}/empty.mscz {get_score_file()}")
+
+
 
 def convert(inname, outname):
     os.system(f'env "{mscore}" {inname} -o {outname}')
@@ -24,29 +46,21 @@ def musescore_open(fname):
 def generate_new_midi():
     global fiter
 
-    convert(get_score_file(), f'{curr_dir}/model_input.mid')
+    convert(get_score_file(), f'{project_dir}/model_input.mid')
     os.system(f"""
-    {curr_dir}/../generate_mid_from_str.py \
+    {curr_dir}/../model/generate_mid_from_str.py \
         --experiment midi \
-        --input "$(python3 {curr_dir}/../MIDI-LLM-tokenizer/midi_to_str.py {curr_dir}/model_input.mid)" \
-        --output out_mid.txt \
+        --input "$(python3 {curr_dir}/../MIDI-LLM-tokenizer/midi_to_str.py {project_dir}/model_input.mid)" \
+        --output {project_dir}/out_mid.txt \
         --max_length 200 \
         --beam_size 2 \
         --cpu \
         -v
     """)
-    os.system(f'python3 {curr_dir}/../MIDI-LLM-tokenizer/str_to_midi.py "$(cat {curr_dir}/out_mid.txt)" --output {curr_dir}/model_output.mid')
+    os.system(f'python3 {curr_dir}/../MIDI-LLM-tokenizer/str_to_midi.py "$(cat {project_dir}/out_mid.txt)" --output {project_dir}/model_output.mid')
 
     fiter += 1
-    convert(f'{curr_dir}/model_output.mid', get_score_file())
-
-if len(sys.argv) > 1:
-    # Initial conversion of the input midi file to the first score file
-    midi_file = sys.argv[1]
-    convert(midi_file, get_score_file())
-else:
-    # If no MIDI file is given, start with empty project.
-    os.system(f"cp {curr_dir}/empty.mscz {get_score_file()}")
+    convert(f'{project_dir}/model_output.mid', get_score_file())
 
 
 print('We are opening the score in MuseScore, only leave the parts that you want to be included in as inspiritation')
@@ -69,4 +83,4 @@ while True:
     if res.lower() not in {'y', 'yes'}:
         break
 
-convert(get_score_file(), '{curr_dir}/output.mid')
+convert(get_score_file(), f'{project_dir}/output.mid')
