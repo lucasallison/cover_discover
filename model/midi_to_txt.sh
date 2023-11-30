a7512a9fbce31c99f9e2e15d3f0336a0e0790b50
lucasallison <lucasgallison@gmail.com>
2023-11-30 12:48:44 +0100

    fixed midi to txt

--- model/midi_to_txt.sh
+++ model/midi_to_txt.sh
@@ -3,9 +3,9 @@
 export LC_CTYPE=C
 export LANG=C
 
-cd ./MIDI-LLM-tokenizer/
+cd ../MIDI-LLM-tokenizer/
 
-for f in $(find ../data/archive -name '*.mid'); do
+for f in $(find ../model/data/archive -name '*.mid'); do
     out="$(echo $f | sed 's;/data/archive/;/data/output/;')"
     mkdir -p $(dirname "$out")
     echo "Processing $f"
