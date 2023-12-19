924efb3148fbf46ecefcb0ccce9ace532da7d446
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2023-12-19 22:42:39 +0100

    setup.sh: download model from my server

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 pip install -r requirements.txt
 curl -L 'https://github.com/musescore/MuseScore/releases/download/v3.6.2/MuseScore-3.6.2.548021370-x86_64.AppImage' > ./app/mscore && chmod +x ./app/mscore
+
+mkdir -p ./model/experiments/midi/
+curl -L 'https://lieuwe.xyz/f/model.ckpt' > ./model/experiments/midi/model.ckpt
